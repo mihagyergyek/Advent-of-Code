daac2aa6429cb877d982157fc8feb25c18adbe97
Docker <docker@example.com>
2022-12-02 15:37:08 +0000

    1. dan brez ponavljanja

--- src/solvers/year_2022/day1.ml
+++ src/solvers/year_2022/day1.ml
@@ -2,16 +2,6 @@ open Solvers.Signature
 open Utils.List_utils
 
 module Solver : Solver = struct
-  let naloga1 data =
-    let lst = List.lines data |> List.map String.trim in
-    let rec list_of_lists (ostali, acc) = function
-      | [] -> ostali
-      | x :: xs ->
-          if String.length x < 1 then list_of_lists (acc :: ostali, 0) xs
-          else list_of_lists (ostali, acc + int_of_string x) xs
-    in
-    list_of_lists ([], 0) lst |> List.maximum |> string_of_int
-
   let sez data =
     let lst = List.lines data |> List.map String.trim in
     let rec list_of_lists (ostali, acc) = function
@@ -21,6 +11,8 @@ module Solver : Solver = struct
           else list_of_lists (ostali, acc + int_of_string x) xs
     in
     list_of_lists ([], 0) lst
+
+  let naloga1 data = sez data |> List.maximum |> string_of_int
 
   let naloga2 data _part1 =
     let palcki = sez data in
